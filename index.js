9a1b373e26e3d64e51211cf08aab66313f870f22
Strider <strange.transistor@gmail.com>
2018-11-06 08:24:08 +0300

    Fix disposer to actually unsubscribe from the event (#5)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = () => {
 	process.stdin.on('keypress', listener);
 
 	return () => {
-		process.stdin.on('keypress', listener);
+		process.stdin.removeListener('keypress', listener);
 
 		if (process.stdin.isTTY) {
 			process.stdin.setRawMode(false);
